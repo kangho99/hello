32b013389cb255d35c6b607756811fd55f71fe83
Kenneth KY <kayoon@coupang.com>
2021-05-07 10:50:20 +0900

    initial commit

---
+++ src/main/java/com/coupang/kenneth/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.coupang.kenneth.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(path = "", method = RequestMethod.GET)
+    public String hello() {
+        return "Hello!";
+    }
+}
